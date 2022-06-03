f80a37a3e806c6132fe9893c859b1b4e47f5a4e2
Jordan Devaney <jordandevaney28@gmail.com>
2022-06-03 13:20:57 -0400

    initial product load into redux

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ export default function Home() {
 	useEffect(() => {
 		const getItems = async () => {
 			const drinks = await getDrinks();
+			console.log("appSync ", drinks);
 			//store in redux
 			dispatch(initialLoad(drinks));
 		};
